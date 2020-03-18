1a8977d63b8d97594d55d1cd72111d77cb129d8a
Mehak1996 <redolence1996@gmail.com>
2020-03-17 22:36:54 -0600

    changed chatterbot

--- eventApp/Views/chatterbotUtility.py
+++ eventApp/Views/chatterbotUtility.py
@@ -16,7 +16,7 @@ class ChatbotUtility():
 		'Event_App_Chatbot',
 		#trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
 		storage_adapter='chatterbot.storage.SQLStorageAdapter',
-		database_uri='mysql://bbf2f3a7c2bdeb:394bee44@us-cdbr-iron-east-04.cleardb.net/heroku_2cd1887254d217c',
+		database_uri='mysql://root:mehak1996@localhost/EventApp',
 		logic_adapters=[
 				{
 					"import_path": "chatterbot.logic.BestMatch",
